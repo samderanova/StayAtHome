c21a65b1e301639dbdc9079dc988f612067e49d9
Sam Der <samder2468@gmail.com>
2021-01-30 10:02:54 -0800

    adding to Feed component

--- client/src/components/Relaxation/Relaxation.js
+++ client/src/components/Relaxation/Relaxation.js
@@ -14,6 +14,7 @@ export default class Relaxation extends React.Component {
         return (
             <div>
                 <h1>This is the relaxation page</h1>
+                <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWVi45nh2EuPP" title="Spotify Playlist" width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
             </div>
         )
     }
--- client/src/components/Trend/Trend.js
+++ client/src/components/Trend/Trend.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 
 export default class Trend extends React.Component {
+    constructor(props) {
+        super(props)
+    }
     componentDidMount() {
-
+        // Make calls to NYT and Trends APIs here using axios
     }
     render() {
         return (
             <div className="Trend">
-                {}
+                {this.props.category === "Trending" 
+                ?
+                <div>
+                    {this.props.category}
+                </div>
+                :
+                <div>
+                    {this.props.category}
+                </div>
+                }
             </div>
         )
     }
