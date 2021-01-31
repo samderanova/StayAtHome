c6e3577612ce8e800716b7f37c07823a66c75e44
Sam Der <samder2468@gmail.com>
2021-01-31 01:35:38 -0800

    finishing up relaxation page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,20 +2,19 @@ import React from 'react'
 import './Home.css'
 
 export default class Home extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-
-        }
-    }
-    componentDidMount() {
-
-    }
     render() {
         return (
-            <div className="Home">
-                <h1>Welcome to the Stay-At-Home Hub!</h1>
-                <p>Whether you're here to relax or catch up on the latest trends, we have the solution for you!</p>
+            <div className="Home-Overall">
+                <div className="Home">
+                    <h1>Welcome to the Stay-At-Home Hub!</h1>
+                    <p>Whether you're here to relax or catch up on the latest trends, we have the solution for you!</p>
+                </div>
+                <p>
+                    Have you ever felt bored during the quarantine?
+                    Have you ever just stared at the ceiling wondering what to do next?
+                    Have you ever looked for that inspiration needed to start your next big project or just relax and take it easy? 
+                    Well, look no further, StayAtHome is your one stop solution against boredom, lack of inspiration and just to flat out relax. 
+                </p>
             </div>
         )
     }
--- client/src/components/Relaxation/Relaxation.js
+++ client/src/components/Relaxation/Relaxation.js
@@ -1,24 +1,54 @@
 import React from 'react'
 import './Relaxation.css'
+import fox from './animal-967657_1920.jpg'
+import breakfast from './breakfast-690128_1920.jpg'
+import coffee from './coffee-3120750_1920.jpg'
+import hedgehog from './hedgehog-468228_1920.jpg'
 
 export default class Relaxation extends React.Component {
     constructor() {
         super()
         this.state = {
-            
+            pics: [
+                <img src={fox} width='50%' height='50%' />,
+                <img src={breakfast} width='50%' height='50%' />,
+                <img src={coffee} width='50%' height='50%' />,
+                <img src={hedgehog} width='50%' height='50%' />
+            ],
+            counter: 0
         }
+        this.updateLeftPic = this.updateLeftPic.bind(this)
+        this.updateRightPic = this.updateRightPic.bind(this)
     }
-    componentDidMount() {
-
+    updateLeftPic() {
+        console.log(this.state.counter)
+        this.setState(prevState => {
+            var newSlideNo = prevState.counter - 1
+            if (newSlideNo < 0) {
+                newSlideNo = 3
+            }
+            return {counter: newSlideNo}
+        })
+    }
+    updateRightPic() {
+        this.setState(prevState => {
+            var newSlideNo = prevState.counter + 1
+            if (newSlideNo > 3) {
+                newSlideNo = 0
+            }
+            return {counter: newSlideNo}
+        })
     }
     render() {
         return (
             <div className="Relaxation">
-                <h1>How about some music?</h1>
-                <div className="playlists">
-                    <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DXdxcBWuJkbcy" title="Motivation"width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-                    <iframe src="https://open.spotify.com/embed/playlist/0vvXsWCC9xrXsKd4FyS8kM" title="LoFi" width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-                    <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWVi45nh2EuPP" title="" width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                <h1>Maybe some pictures to help calm you down?</h1>
+                <div className="pictures">
+                    {this.state.pics[this.state.counter]}
+                </div>
+                <div className="arrows">
+                    <span onClick={() => this.updateLeftPic()}>{'<'}</span>
+                    <span onClick={() => this.updateRightPic()}>{'>'}</span>
                 </div>
             </div>
         )
